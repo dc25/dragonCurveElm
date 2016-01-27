ed1014139c5e0d4958c986e9e3cae858051150ec
Dave Compton <davecompton7@gmail.com>
2016-01-27 18:03:30 +0000

    better naming

--- T.elm
+++ T.elm
@@ -5,53 +5,75 @@ import Time exposing (..)
 
 type alias Point = (Float, Float)
 
-type alias Dragon =
+type alias Model =
     { points : List Point
     , level : Int
+    , frame : Int
     }
 
-init : Dragon
+maxLevel = 12
+frameCount = 100
+
+init : Model
 init = { points = [(-200.0, 0.0), (200.0, 0.0)]
        , level = 0 
+       , frame = 0
        }
 
 type Side = Left | Right
 
 otherSide : Side -> Side
-otherSide s = 
-    case s of 
+otherSide side = 
+    case side of 
         Left -> Right
         Right -> Left
-        
 
-bend : Point -> Point -> Side -> Point
-bend  (x0,y0) (x1,y1) s = 
-    let (vx, vy) = ((x1 - x0) / 2.0, (y1 - y0) / 2.0)
-        (mx, my) = (x0 + vx, y0 + vy)
-        (dx, dy) = case s of 
-                      Left -> (-vy, vx)
-                      Right -> (vy, -vx)
-    in  (mx + dx, my + dy)
+newPoint : Point -> Point -> Side -> Float -> Point
+newPoint  (x0,y0) (x1,y1) side progress = 
+    let (vx, vy) = ((x1 - x0) / 2.0, (y1 - y0) / 2.0) 
+        (mx, my) = (x0 + vx, y0 + vy)  -- midpoint
+        (dx, dy) = case side of 
+                      Left -> (-vy * progress , vx * progress )
+                      Right -> (vy * progress , -vx * progress )
+    in  (mx + dx, my + dy) --offset from midpoint to left or right 
 
-evolve : List Point -> Side -> List Point
-evolve points side = case points of
+-- Insert between existing points. Offset to left or right side.
+newPoints' : Side -> Float -> List Point -> List Point
+newPoints' side progress points = 
+  case points of
     [] -> []
     [p0] -> [p0]  
-    p0::p1::rest -> p0 :: bend p0 p1 side :: evolve (p1::rest) (otherSide side)
+    p0::p1::rest -> p0 :: newPoint p0 p1 side progress :: newPoints' (otherSide side) progress (p1::rest)
 
-next : Dragon -> Dragon
-next dragon = 
-    { points = evolve dragon.points Left
-    , level = dragon.level+1
-    }
+newPoints : Float -> List Point -> List Point
+newPoints progress points = newPoints' Left progress points
 
-main =
-  let dragon = Signal.foldp (\_ d -> next d) init (every (100*millisecond))
-  in Signal.filter (\d -> d.level < 14) init dragon
-     |> Signal.map view 
+update : Model -> Model
+update model = 
+    let nextFrame = model.frame + 1
+    in if (nextFrame == frameCount) then
+            { points = newPoints 1.0 model.points 
+            , level = model.level+1
+            , frame = 0
+            }
+       else
+            { model | frame = nextFrame
+            }
 
-view dragon = layers
-            [ collage 700 700 
-              [ dragon.points |> path |> traced (solid orange) ]
-              , show (toString dragon.level)
+view model = 
+    let progress = (toFloat (model.frame)/toFloat frameCount)
+    in layers
+          [ collage 700 700 
+            [ model.points 
+                |> newPoints progress
+                |> path 
+                |> traced (solid purple) 
             ]
+            , show model.level
+          ]
+main =
+  Signal.foldp (\_ c -> c+1) 0 (every (5*millisecond))
+     |> Signal.filter (\c -> c < maxLevel*frameCount+1) 0 
+     |> Signal.foldp (\_ d -> update d) init 
+     |> Signal.map view
+
